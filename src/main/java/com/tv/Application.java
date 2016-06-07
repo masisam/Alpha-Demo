b64fec4723697078ac6bd0ed84650400c676253f
masisam <urmamasucks@gmail.com>
2016-06-07 15:53:01 -0700

    LICENSE and README added.

--- src/main/java/com/tv/Application.java
+++ src/main/java/com/tv/Application.java
@@ -3,6 +3,11 @@ package com.tv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+/**
+ * @author masisam
+ *
+ */
 @SpringBootApplication
 public class Application {
 
